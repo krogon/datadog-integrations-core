dc3a3d17d386fcc5072d7d836b44ca2b04666631
Florent Clarret <florent.clarret@datadoghq.com>
2022-07-20 15:21:50 +0200

    Add the docker compose file and set up the e2e test

--- impala/tests/conftest.py
+++ impala/tests/conftest.py
@@ -1,9 +1,31 @@
 # (C) Datadog, Inc. 2022-present
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
+import os
+
 import pytest
+
+from datadog_checks.dev import docker_run, load_jmx_config
+from datadog_checks.dev.conditions import CheckDockerLogs
+
+from .common import HERE
 
 
 @pytest.fixture(scope='session')
 def dd_environment():
-    yield {}, {'use_jmx': True}
+    compose_file = os.path.join(HERE, 'compose', 'docker-compose.yaml')
+    with docker_run(
+        compose_file,
+        conditions=[
+            CheckDockerLogs(
+                identifier=compose_file,
+                patterns="Connected to metastore.",
+                matches='all',
+                wait=5,
+                attempts=20,
+            )
+        ],
+    ):
+        config = load_jmx_config()
+        config['instances'] = []
+        yield config, {'use_jmx': True}
